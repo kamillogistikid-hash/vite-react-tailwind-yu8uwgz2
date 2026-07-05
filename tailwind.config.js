51099627ed76591b24ebc2f01eaeadaf9e06d338
kamillogistikid-hash <kamillogistik.id@gmail.com>
2026-07-05 13:45:55 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
